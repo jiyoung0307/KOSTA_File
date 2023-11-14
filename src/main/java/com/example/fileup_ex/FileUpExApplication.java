b8c2cfbd95415cd21dcff70c14841f86f142763d
zy_zero <jiyoungjang0307@gmail.com>
2023-11-14 13:36:43 +0900

    fileupload

---
+++ src/main/java/com/example/fileup_ex/FileUpExApplication.java
@@ -0,0 +1,13 @@
+package com.example.fileup_ex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileUpExApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileUpExApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/fileup_ex/controller/FileUploadController.java
@@ -0,0 +1,43 @@
+package com.example.fileup_ex.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.Part;
+import java.io.IOException;
+import java.util.Collection;
+
+/** 타입리프로 할 경우 Controller 작성 */
+@Controller
+@RequestMapping("/files")
+public class FileUploadController {
+    @Value("${file.dir}")
+    private String fileDir;
+
+    @GetMapping("/upload")
+    public String upload() {
+        return "FileUploadForm";
+    }
+
+    @PostMapping("/upload")
+    public String upload(HttpServletRequest req) throws ServletException, IOException {
+        System.out.println(req.getParameter("name"));
+        req.getParameter("name");
+        req.getParameter("price");
+        req.getParameter("quantity");
+        Collection<Part> parts = req.getParts();
+        for(Part part : parts) {
+            System.out.println(part.getName());
+            System.out.println(part.getSize());
+            System.out.println(part.getContentType());
+            System.out.println(part.getSubmittedFileName());
+            part.write(fileDir + part.getSubmittedFileName());
+        }
+        return "FileUploadForm";
+    }
+}

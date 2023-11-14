b89f5d5cb2d969cd25e592e056b0941de8c4d562
zy_zero <jiyoungjang0307@gmail.com>
2023-11-14 14:28:54 +0900

    v2.2

--- src/main/java/com/example/fileup_ex/controller/FileUploadController.java
+++ src/main/java/com/example/fileup_ex/controller/FileUploadController.java
@@ -1,20 +1,27 @@
 package com.example.fileup_ex.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.Part;
+import java.io.File;
 import java.io.IOException;
 import java.util.Collection;
 
-/** 타입리프로 할 경우 Controller 작성 */
+/**
+ * 타입리프로 할 경우 Controller 작성
+ */
 @Controller
 @RequestMapping("/files")
+@Slf4j
 public class FileUploadController {
     @Value("${file.dir}")
     private String fileDir;
@@ -25,17 +32,19 @@ public class FileUploadController {
     }
 
     @PostMapping("/upload")
-    public String upload(HttpServletRequest req) throws ServletException, IOException {
-        System.out.println(req.getParameter("name"));
-        req.getParameter("price");
-        req.getParameter("quantity");
-        Collection<Part> parts = req.getParts();
-        for(Part part : parts) {
-            System.out.println(part.getName());
-            System.out.println(part.getSize());
-            System.out.println(part.getContentType());
-            System.out.println(part.getSubmittedFileName());
-            part.write(fileDir + part.getSubmittedFileName());  /** images에 null이 생기는 이유 */
+    public String upload(@RequestParam String name,
+                         @RequestParam Integer price,
+                         @RequestParam Integer quantity,
+                         @RequestParam MultipartFile file,
+                         HttpServletRequest req) throws ServletException, IOException {
+        System.out.println(name);
+
+        /** 파일을 들고와보자 */
+        log.info("product name {} ==> ", name);
+        log.info("file {} ==> ", file);
+
+        if (!file.isEmpty()) {
+            file.transferTo(new File(fileDir + file.getOriginalFilename()));
         }
         return "FileUploadForm";
     }

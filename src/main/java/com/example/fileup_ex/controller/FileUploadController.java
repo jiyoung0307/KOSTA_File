1ef7f739e7d38a0e815acbb3b1d3d53cb06816f7
zy_zero <jiyoungjang0307@gmail.com>
2023-11-14 14:52:48 +0900

    여러 개의 파일 업로드까지

--- src/main/java/com/example/fileup_ex/controller/FileUploadController.java
+++ src/main/java/com/example/fileup_ex/controller/FileUploadController.java
@@ -1,14 +1,13 @@
 package com.example.fileup_ex.controller;
 
+import com.example.fileup_ex.entity.Product;
+import com.example.fileup_ex.entity.ProductDTO;
 import com.example.fileup_ex.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.ServletException;
@@ -17,6 +16,7 @@ import javax.servlet.http.Part;
 import java.io.File;
 import java.io.IOException;
 import java.util.Collection;
+import java.util.List;
 
 /** 타입리프로 할 경우 Controller 작성 */
 @Controller
@@ -35,20 +35,20 @@ public class FileUploadController {
     }
 
     @PostMapping("/upload")
-    public String upload(@RequestParam String name,
-                         @RequestParam Integer price,
-                         @RequestParam Integer quantity,
-                         @RequestParam MultipartFile file,
-                         HttpServletRequest req) throws ServletException, IOException {
-        System.out.println(name);
-
+    public String upload(@ModelAttribute ProductDTO productDTO){
         /** 파일을 들고와보자 */
-        log.info("product name {} ==> ", name);
-        log.info("file {} ==> ", file);
+        log.info("product name {} ==> ", productDTO.getName());
+        List<MultipartFile> imageFiles = productDTO.getImageFiles();
+        log.info("file {} ==> ", productDTO.getImageFiles());
 
-        if (!file.isEmpty()) {
-            file.transferTo(new File(fileDir + file.getOriginalFilename()));
+        /** 여러 개의 파일을 들고오는지 보자 */
+        for(MultipartFile imageFile : imageFiles) {
+            log.info("file {} ==> ", imageFile.getOriginalFilename());
         }
+
+//        if (!file.isEmpty()) {
+//            file.transferTo(new File(fileDir + file.getOriginalFilename()));
+//        }
         return "FileUploadForm";
     }
 }
